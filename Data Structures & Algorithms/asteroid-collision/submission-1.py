70c7d2b8f0b899c131e83129610f139b927d19c0
itsdishank <65216508+itsdishank@users.noreply.github.com>
2026-06-19 15:21:09 -0400

    Add: asteroid-collision - submission-1

---
+++ Data Structures & Algorithms/asteroid-collision/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = [asteroids[0]]
+        for i in range(1, len(asteroids)):
+            stack.append(asteroids[i])
+            while len(stack)>=2 and stack[-1]<0 and stack[-2]>0:
+                ast1 = stack.pop()
+                ast2 = stack.pop()
+                if abs(ast1) > ast2:
+                    stack.append(ast1)
+                elif abs(ast1) < ast2:
+                    stack.append(ast2)
+        return stack
